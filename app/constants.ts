7c64a7e8337ab1d9631163a4117df4bb65b678ef
JUBAYIR69 <jubayir82@gmail.com>
2026-06-30 23:54:35 +0600

    Update contract address and modify ABI inputs
    Updated the contract address and modified the ABI to include additional inputs for functions.

--- app/constants.ts
+++ app/constants.ts
@@ -1,26 +1,33 @@
-export const CONTRACT_ADDRESS = "0xF20F1edB82ca4085b9251b960829631458e6d98e";
+export const CONTRACT_ADDRESS = "0x15Fe10571dD8EE08eADFDf7756219acF67BE09E8";
 
 export const CONTRACT_ABI = [
   {
     "name": "post_job",
     "type": "function",
-    "inputs": [{ "name": "desc", "type": "string" }],
-    "outputs": [{ "type": "string" }]
+    "inputs": [
+      { "name": "desc", "type": "string" },
+      { "name": "client", "type": "string" }
+    ],
+    "outputs": []
   },
   {
     "name": "submit_work",
     "type": "function",
     "inputs": [
       { "name": "job_id", "type": "string" },
-      { "name": "url", "type": "string" }
+      { "name": "url", "type": "string" },
+      { "name": "freelancer", "type": "string" }
     ],
-    "outputs": [{ "type": "string" }]
+    "outputs": []
   },
   {
     "name": "approve_work",
     "type": "function",
-    "inputs": [{ "name": "job_id", "type": "string" }],
-    "outputs": [{ "type": "string" }]
+    "inputs": [
+      { "name": "job_id", "type": "string" },
+      { "name": "approver", "type": "string" }
+    ],
+    "outputs": []
   },
   {
     "name": "get_all_jobs",
