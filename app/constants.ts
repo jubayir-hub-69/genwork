dac758061784ec3adde77a15f71e0e1905d254a1
JUBAYIR69 <jubayir82@gmail.com>
2026-08-05 18:00:34 +0600

    Update function names and inputs in constants

--- app/constants.ts
+++ app/constants.ts
@@ -6,8 +6,7 @@ export const CONTRACT_ABI = [
     "type": "function",
     "inputs": [
       { "name": "desc", "type": "string" },
-      { "name": "price", "type": "string" },
-      { "name": "client", "type": "string" }
+      { "name": "category", "type": "string" }
     ],
     "outputs": []
   },
@@ -16,26 +15,7 @@ export const CONTRACT_ABI = [
     "type": "function",
     "inputs": [
       { "name": "job_id", "type": "string" },
-      { "name": "url", "type": "string" },
-      { "name": "freelancer", "type": "string" }
-    ],
-    "outputs": []
-  },
-  {
-    "name": "approve_work",
-    "type": "function",
-    "inputs": [
-      { "name": "job_id", "type": "string" },
-      { "name": "approver", "type": "string" }
-    ],
-    "outputs": []
-  },
-  {
-    "name": "reject_work",
-    "type": "function",
-    "inputs": [
-      { "name": "job_id", "type": "string" },
-      { "name": "approver", "type": "string" }
+      { "name": "work_url", "type": "string" }
     ],
     "outputs": []
   },
@@ -49,7 +29,47 @@ export const CONTRACT_ABI = [
     "outputs": []
   },
   {
+    "name": "reject_work",
+    "type": "function",
+    "inputs": [
+      { "name": "job_id", "type": "string" }
+    ],
+    "outputs": []
+  },
+  {
+    "name": "cancel_job",
+    "type": "function",
+    "inputs": [
+      { "name": "job_id", "type": "string" }
+    ],
+    "outputs": []
+  },
+  {
+    "name": "send_message",
+    "type": "function",
+    "inputs": [
+      { "name": "job_id", "type": "string" },
+      { "name": "message", "type": "string" }
+    ],
+    "outputs": []
+  },
+  {
+    "name": "update_profile",
+    "type": "function",
+    "inputs": [
+      { "name": "nickname", "type": "string" },
+      { "name": "avatar_url", "type": "string" }
+    ],
+    "outputs": []
+  },
+  {
     "name": "get_all_jobs",
+    "type": "function",
+    "inputs": [],
+    "outputs": [{ "type": "string" }]
+  },
+  {
+    "name": "get_profiles",
     "type": "function",
     "inputs": [],
     "outputs": [{ "type": "string" }]
